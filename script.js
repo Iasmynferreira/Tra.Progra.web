7e2905915c5b1ce8e0ee9e8b31c06a8720fbc440
Iasmynferreira <iasmynferreira96@gmail.com>
2024-04-04 07:43:43 -0300

    trab.2

---
+++ script.js
@@ -0,0 +1,12 @@
+function calcularNUM(){
+
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    
+    var NUM = (num1 + num2).toFixed(1);
+    
+    
+   
+
+
+}
